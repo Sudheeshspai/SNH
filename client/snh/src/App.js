f755b4f56b1b95f75d0e0cb1002b571d3a28cbd1
psychoSherlock <pschosherlock@gmail.com>
2023-09-16 21:31:38 +0530

    Finished

--- client/snh/src/App.js
+++ client/snh/src/App.js
@@ -7,10 +7,20 @@ function App() {
       <div className="top-heading">
         <h1>Nothing to See Here</h1>
         <h1 className="thanks">
-          Thanks for using NSH. More features will be added soon.
+          Thanks for using NSH. <br></br> We will be delivering amazing new
+          features soon
         </h1>
       </div>
       <Terminal />
+      <footer class="footer">
+        <a
+          target="_blank"
+          href="https://github.com/Sudheeshspai/SNH"
+          class="underline"
+        >
+          Made with 💙 by Fsociety
+        </a>
+      </footer>
     </div>
   );
 }
